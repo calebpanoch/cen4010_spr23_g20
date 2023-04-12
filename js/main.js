bdc4244abe44b8de96d0db777c57d7f52bf152ff
cpanoch2018 <123989314+calebpanoch@users.noreply.github.com>
2023-04-12 19:21:55 -0400

    Update index.html and main.js
    Made Main Feed
    
    Have to remove old templates

--- js/main.js
+++ js/main.js
@@ -4,31 +4,13 @@ Parse.serverURL = "https://parseapi.back4app.com/";
 
 const currentUser = Parse.User.current();
 
-if (currentUser) {
-    console.log(currentUser)
-    console.log(currentUser.getUsername())
-    
+if (currentUser) {    
     document.getElementById("username").innerHTML = currentUser.getUsername()
 } else {
     location.href = 'login.html';
 }
 
-
 var Posts = Parse.Object.extend("Posts");
-
-/*function create() {
-    mypet = new Pet();
-    mypet.set("caption", textName);
-
-    mypet.save().then(function(pet){
-         console.log('Pet created successful with name: ' + pet.get("name") + ' and age: ' + pet.get("agePet"));
-    }).catch(function(error){
-         console.log('Error: ' + error.message);
-    });
-}
-
-create();
-*/
 
 function read() {
     query = new Parse.Query(Posts);
@@ -46,30 +28,6 @@ function read() {
     });
 }
 
-
-// Create a new User
-/*async function createParseUser() {
-  // Creates a new Parse "User" object, which is created by default in your Parse app
-  let user = new Parse.User();
-  // Set the input values to the new "User" object
-  user.set("username", document.getElementById("username").value);
-  user.set("email", document.getElementById("email").value);
-  user.set("password", document.getElementById("password").value);
-  try {
-    // Call the save method, which returns the saved object if successful
-    user = await user.save();
-    if (user !== null) {
-      // Notify the success by getting the attributes from the "User" object, by using the get method (the id attribute needs to be accessed directly, though)
-      alert(
-        `New object created with success! ObjectId: ${
-          user.id
-        }, ${user.get("username")}`
-      );
-    }
-  } catch (error) {
-    alert(`Error: ${error.message}`);
-  }
-}*/
 function logout() {
     Parse.User.logOut()
     location.href = 'login.html'
@@ -78,4 +36,65 @@ function logout() {
 // Add on click listener to call the create parse user function
 document.getElementById("logout").addEventListener("click", async function () {
   logout();
+});
+
+var newPost = `<div  class="center" align="center" style="margin: auto; padding-top: 2%; padding-bottom: 2%; max-width: 50%;">
+                    <div class="bg-white bg-opacity-0 text-black contents"> 
+                        <div class="pfp">
+                            <img src="./assets/pfp.jpg" style="max-width: 3%;">
+                            <p style="padding-left: 10px">Username</p>
+                        </div>
+                        <h2 class="text-3xl font-serif text-center"style="padding-bottom: 2%;">
+                        TITLEHERE
+                        </h2>
+                        <div class="center">
+                        <img src=IMGSRCHERE style="padding: 1%">
+
+                        </div> 
+                        <div class=" lg:flex items-center ml-auto text-pink">
+                            <ul class="flex space-x-8">
+                                <li>  <button onclick="displayLike()">Like</button> 
+                                <p id="liked"></p> </li>
+                                <li>  <button onclick="Repost()">Repost</button> 
+                                <p id="reposted"></p> </li>
+                                <li>  <button onclick="Share()">Share</button> 
+                                <p id="shared"></p> </li>
+                                <li>  <button onclick="uploadPost()">Upload</button> 
+                                <p id="uploadPost"></p> </li>
+
+                            </ul>
+                        </div>
+
+                        <h3 class="font-sans text-lg"style="padding: 1%;"> CAPTIONHERE
+                        </h3>
+                    </div>
+                </div>`
+
+var postNum = 0
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       
+        query = new Parse.Query(Posts);
+        query.addDescending("createdAt")
+        query.limit(5)
+        query.skip(postNum)
+        postNum = postNum + 5
+        postList = query.find()
+        console.log(postList)
+        query.find().then(function(results) {
+            console.log()
+            for (let step = 0; step < 5; step++) {
+              // Runs 5 times, with values of step 0 through 4.
+              console.log(results[step].get("image"))
+                
+               replaceTitle = newPost.replace("TITLEHERE", results[step].get("title"))
+               replaceCaption = replaceTitle.replace("CAPTIONHERE", results[step].get("caption"))
+               replaceIMG = replaceCaption.replace("IMGSRCHERE", results[step].get("image")._url)
+
+               $('#posts').append(replaceIMG);
+            }
+        })
+       
+   }
 });
