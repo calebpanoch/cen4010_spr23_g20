5386b03cf0b662f7b1ca61d10fe073d2652c1d28
cpanoch2018 <123989314+calebpanoch@users.noreply.github.com>
2023-04-14 17:07:28 -0400

    Update main.js & index.html
    Made comment functionality
    Made delete post functionality

--- js/main.js
+++ js/main.js
@@ -12,22 +12,7 @@ if (currentUser) {
 }
 
 var Posts = Parse.Object.extend("Posts");
-
-function read() {
-    query = new Parse.Query(Posts);
-    console.log(document.getElementById("PostCaption").value)
-    query.equalTo("caption", document.getElementById("PostCaption").value);
-    query.first().then(function(caption){ 
-        if(caption){
-            document.getElementById("display").innerHTML = "Post Age: " + caption.get("updatedAt")
-            document.getElementById("image").src = caption.get("image")._url
-        } else {
-           console.log("Nothing found, please try again");
-        }
-    }).catch(function(error){
-        console.log("Error: " + error.code + " " + error.message);       
-    });
-}
+var Comments = Parse.Object.extend("Comments");
 
 function logout() {
     Parse.User.logOut()
@@ -41,30 +26,57 @@ document.getElementById("logout").addEventListener("click", async function () {
 
 function likePost(objectId) {
     if (!event) {
-        event = window.event; // Older versions of IE use 
-                              // a global reference 
-                              // and not an argument.
+        event = window.event; 
     };
 
-    var el = (event.target || event.srcElement); // DOM uses 'target';
-                                                 // older versions of 
-                                                 // IE use 'srcElement'
-    queryTest = new Parse.Query(Posts);
-    queryTest.equalTo("objectId",objectId)
-    queryTest.first().then(function(result) {
+    var el = (event.target || event.srcElement);
+    queryObj = new Parse.Query(Posts);
+    queryObj.equalTo("objectId",objectId)
+    queryObj.first().then(function(result) {
         if (result.get("likes").find(x => x==currentUsername) == null) {
             result.addUnique("likes",currentUsername)
             result.save()
             el.innerHTML = "Likes " + result.get("likes").length + " &#x1F44D"
         }
-
     });
-    
-    
 }
 
-var newPost = `<div  class="center" align="center" style="margin: auto; padding-top: 2%; padding-bottom: 2%; max-width: 50%;">
+function deletePost(objectId) {
+    if (confirm("Are you sure you want to delete this post?") == true) {
+        queryObj = new Parse.Query(Posts);
+        queryObj.equalTo("objectId",objectId)
+        queryObj.first().then(function(result) {
+            result.destroy();
+            location.reload();
+        });
+    }
+}
+
+function postComment(objectId) {
+    comment = document.getElementById(objectId+"comment").value
+    if (comment != "") {
+        queryObj = new Parse.Query(Posts);
+        queryObj.equalTo("objectId",objectId)
+        queryObj.first().then(function(result) {
+            /*newComment = new Comments();
+            newComment.set("post",objectId)
+            newComment.set("author",currentUsername)
+            newComment.set("text",comment)
+            newComment.save()*/
+            //result.addUnique("comments",newComment)
+            result.addUnique("comments",currentUsername+": " + comment)
+            result.save()
+            location.reload();
+        });
+    }
+}
+
+var newPost = `
+              <div  class="center" align="center" style="margin: auto; padding-top: 2%; padding-bottom: 2%; max-width: 50%;">
                     <div class="bg-white bg-opacity-0 text-black contents"> 
+                        <div class=" lg:flex items-center ml-auto text-pink">
+                            <button HIDDENHERE onclick="deletPost('POSTIDHERE')" class="bg-indigo-500 hover:bg-indigo-100 text-black font-bold py-1 px-1 rounded">Delete 🗑️</button>
+                        </div>
                         <div class="pfp">
                             <img src="./assets/pfp.jpg" style="max-width: 3%;">
                             <p style="padding-left: 10px">USERNAMEHERE</p>
@@ -91,11 +103,27 @@ var newPost = `<div  class="center" align="center" style="margin: auto; padding-
 
                         <h3 class="font-sans text-lg"style="padding: 1%;"> CAPTIONHERE
                         </h3>
+                        <h4 class = "font-sans text-lg text-bold text-left"><b>Comments:</b></h4>
+                        <div id="comments" >COMMENTSHERE</div>
+                        <input placeholder="Write a comment..." type="text" id="POSTIDHEREcomment">
+                        <button onclick="postComment('POSTIDHERE')">Send</button>
                     </div>
-                </div>`
+                </div>
+`
 
 var postNum = 0
 query = new Parse.Query(Posts);
+
+/*function makeComments(comments, step2) {
+    if (step2 < comments.length) {
+        queryObj = new Parse.Query(Comments);
+        queryObj.equalTo("objectId",comments[step2].id)
+        queryObj.first().then(function(result) {
+            newComment = '<h5 class = "font-sans text-lg text-bold text-left">' + result.get("author") + ": " + result.get("text") + '</h5>'
+            return newComment + makeComments(comments, step2+1);
+        })
+    }
+}*/
 
 function loadPosts(numPosts) {
     query.addDescending("createdAt")
@@ -106,14 +134,34 @@ function loadPosts(numPosts) {
     query.find().then(function(results) {
         for (let step = 0; step < numPosts; step++) {
           // Runs 5 times, with values of step 0 through 4.
-           replaceUsername = newPost.replace("USERNAMEHERE", results[step].get("poster"))
-           replaceTitle = replaceUsername.replace("TITLEHERE", results[step].get("title"))
-           replaceCaption = replaceTitle.replace("CAPTIONHERE", results[step].get("caption"))
-           replaceImg = replaceCaption.replace("IMGSRCHERE", results[step].get("image")._url)
-            replaceLikes = replaceImg.replace("NUMLIKESHERE", results[step].get("likes").length)
-            replacePostId = replaceLikes.replace("POSTIDHERE", results[step].id)
+           replaceUsername = newPost.replaceAll("USERNAMEHERE", results[step].get("poster"))
+           replaceTitle = replaceUsername.replaceAll("TITLEHERE", results[step].get("title"))
+           replaceCaption = replaceTitle.replaceAll("CAPTIONHERE", results[step].get("caption"))
+           replaceImg = replaceCaption.replaceAll("IMGSRCHERE", results[step].get("image")._url)
+            replaceLikes = replaceImg.replaceAll("NUMLIKESHERE", results[step].get("likes").length)
+            replacePostId = replaceLikes.replaceAll("POSTIDHERE", results[step].id)
+            
+            if (results[step].get("poster") == currentUsername) {
+                replaceHidden = replacePostId.replaceAll("HIDDENHERE", "")
+            }
+            else {
+                replaceHidden = replacePostId.replaceAll("HIDDENHERE", "hidden")
+            }
+            // Add comments here
+            let newComments = ""
+            comments = results[step].get("comments")
+            if (comments.length > 0) {
+                for (let step2 = 0; step2 < comments.length; step2++) {
+                    newComments = newComments + '<h5 class = "font-sans text-lg text-bold text-left">' + comments[step2] + '</h5>'
+                }
+                replaceComments = replaceHidden.replace("COMMENTSHERE",newComments)
+                $('#posts').append(replaceComments);
+            }
+            else {
+                replaceHidden = replaceHidden.replace("COMMENTSHERE","")
+                $('#posts').append(replaceHidden);
+            }
 
-           $('#posts').append(replacePostId);
         }
     });
 }
