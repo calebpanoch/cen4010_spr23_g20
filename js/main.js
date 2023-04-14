1fd8be1e8f2fa8e97f3ac27bfbf8711103575eb8
cpanoch2018 <123989314+calebpanoch@users.noreply.github.com>
2023-04-14 13:03:33 -0400

    Update main.js & index.html
    Added liking functionality

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const currentUser = Parse.User.current();
 
 if (currentUser) {    
     document.getElementById("username").innerHTML = currentUser.getUsername()
+    currentUsername = currentUser.getUsername()
 } else {
     location.href = 'login.html';
 }
@@ -38,6 +39,30 @@ document.getElementById("logout").addEventListener("click", async function () {
   logout();
 });
 
+function likePost(objectId) {
+    if (!event) {
+        event = window.event; // Older versions of IE use 
+                              // a global reference 
+                              // and not an argument.
+    };
+
+    var el = (event.target || event.srcElement); // DOM uses 'target';
+                                                 // older versions of 
+                                                 // IE use 'srcElement'
+    queryTest = new Parse.Query(Posts);
+    queryTest.equalTo("objectId",objectId)
+    queryTest.first().then(function(result) {
+        if (result.get("likes").find(x => x==currentUsername) == null) {
+            result.addUnique("likes",currentUsername)
+            result.save()
+            el.innerHTML = "Likes " + result.get("likes").length + " &#x1F44D"
+        }
+
+    });
+    
+    
+}
+
 var newPost = `<div  class="center" align="center" style="margin: auto; padding-top: 2%; padding-bottom: 2%; max-width: 50%;">
                     <div class="bg-white bg-opacity-0 text-black contents"> 
                         <div class="pfp">
@@ -54,7 +79,7 @@ var newPost = `<div  class="center" align="center" style="margin: auto; padding-
                          
                         <div class=" lg:flex items-center ml-auto text-pink">
                             <ul class="flex space-x-8">
-                                <li>  <button onclick="displayLike()" class="bg-indigo-500 hover:bg-indigo-100 text-black font-bold py-1 px-1 rounded">Like &#x1F44D</button>
+                                <li>  <button onclick="likePost('POSTIDHERE')" class="bg-indigo-500 hover:bg-indigo-100 text-black font-bold py-1 px-1 rounded">Likes NUMLIKESHERE &#x1F44D</button>
                                 <p id="liked"></p> </li>
                                 <li>  <button onclick="Repost()" class="bg-indigo-500 hover:bg-indigo-100 text-black font-bold py-1 px-1 rounded">Repost &#x1F501</button> 
                                 <p id="reposted"></p> </li>
@@ -72,44 +97,32 @@ var newPost = `<div  class="center" align="center" style="margin: auto; padding-
 var postNum = 0
 query = new Parse.Query(Posts);
 
-query.addDescending("createdAt")
-query.limit(5)
-query.skip(postNum)
-postNum = postNum + 5
-postList = query.find()
-query.find().then(function(results) {
-    for (let step = 0; step < 5; step++) {
-      // Runs 5 times, with values of step 0 through 4.                
-       replaceUsername = newPost.replace("USERNAMEHERE", results[step].get("poster"))
-       replaceTitle = replaceUsername.replace("TITLEHERE", results[step].get("title"))
-       replaceCaption = replaceTitle.replace("CAPTIONHERE", results[step].get("caption"))
-       replaceIMG = replaceCaption.replace("IMGSRCHERE", results[step].get("image")._url)
+function loadPosts(numPosts) {
+    query.addDescending("createdAt")
+    query.limit(numPosts)
+    query.skip(postNum)
+    postNum = postNum + numPosts
+    postList = query.find()
+    query.find().then(function(results) {
+        for (let step = 0; step < numPosts; step++) {
+          // Runs 5 times, with values of step 0 through 4.
+           replaceUsername = newPost.replace("USERNAMEHERE", results[step].get("poster"))
+           replaceTitle = replaceUsername.replace("TITLEHERE", results[step].get("title"))
+           replaceCaption = replaceTitle.replace("CAPTIONHERE", results[step].get("caption"))
+           replaceImg = replaceCaption.replace("IMGSRCHERE", results[step].get("image")._url)
+            replaceLikes = replaceImg.replace("NUMLIKESHERE", results[step].get("likes").length)
+            replacePostId = replaceLikes.replace("POSTIDHERE", results[step].id)
 
-       $('#posts').append(replaceIMG);
-    }
-});
+           $('#posts').append(replacePostId);
+        }
+    });
+}
+
+loadPosts(5);
 
 //window scroll function
 $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        
-         
-         query.addDescending("createdAt")
-         query.limit(5)
-         query.skip(postNum)
-         postNum = postNum + 5
-         postList = query.find()
-         query.find().then(function(results) {
-             for (let step = 0; step < 5; step++) {
-               // Runs 5 times, with values of step 0 through 4.                
-                repalceUsername = newPost.replace("USERNAMEHERE", results[step].get("poster"))
-                replaceTitle = repalceUsername.replace("TITLEHERE", results[step].get("title"))
-                replaceCaption = replaceTitle.replace("CAPTIONHERE", results[step].get("caption"))
-                replaceIMG = replaceCaption.replace("IMGSRCHERE", results[step].get("image")._url)
- 
-                $('#posts').append(replaceIMG);
-             }
-         })
-        
+        loadPosts(5);
     }
  });
